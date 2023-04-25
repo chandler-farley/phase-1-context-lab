152787b490bdb521a7b23860a4c911390a1389a4
Chandler Farley <chandlerfarley@live.com>
2023-04-25 16:43:25 -0500

    complete lab

--- index.js
+++ index.js
@@ -21,3 +21,61 @@ const allWagesFor = function () {
     return payable
 }
 
+
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord)
+}
+
+const createTimeInEvent = function(timeIn) {
+    let time = {
+        type: "TimeIn",
+        hour: parseInt(timeIn.split(" ")[1]),
+        date: timeIn.split(" ")[0]
+    }
+    this.timeInEvents.push(time)
+    return this
+}
+
+const createTimeOutEvent = function(timeOut) {
+    let time = {
+        type: "TimeOut",
+        hour: parseInt(timeOut.split(" ")[1]),
+        date: timeOut.split(" ")[0]
+    }
+    this.timeOutEvents.push(time)
+    return this
+}
+
+const hoursWorkedOnDate = function(date) {
+    let timeInHour = this.timeInEvents.filter(obj => obj.date === date).map(ele => ele.hour)
+    let timeOutHour = this.timeOutEvents.filter(obj => obj.date === date).map(ele => ele.hour)
+    let hoursWorked = timeOutHour.map((value, i) => value - timeInHour[i]) / 100
+    return hoursWorked
+}
+
+const wagesEarnedOnDate = function(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(obj => obj.firstName === firstName)
+}
+
+function calculatePayroll(array) {
+    let payStubs = []
+    array.forEach(emp => payStubs.push(allWagesFor.call(emp)))
+    let payRoll = payStubs.reduce((accum, curr) => 
+    accum + curr, 0)
+    return payRoll
+}
